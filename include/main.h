e40adee096bca412228ebe2ef12b1b9886ccd98a
007TheSpectre <jean-baptiste.lamy-floret@epitech.eu>
2024-09-12 14:48:00 +0200

    [+] include/

---
+++ include/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_
+    #define MAIN_H_
+
+    #include <unistd.h>
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include "struct.h"
+
+    #endif /* MAIN_H_ */
---
+++ include/struct.h
@@ -0,0 +1,8 @@
+#ifndef STRUCT_H_
+    #define STRUCT_H_
+
+typedef struct data_s {
+    int **sudoku_map;
+} data_t;
+
+    #endif /* STRUCT_H_ */
